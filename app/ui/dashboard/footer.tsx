d18bea21e720a58454c1e0ba1f4e68377b4c362f
Mamad Ahmad <mamadahmadfnc@gmail.com>
2025-07-19 14:45:25 +0700

    Tugas Pertemuan 5 membuat CRUD Database Postgres

--- app/ui/dashboard/footer.tsx
+++ app/ui/dashboard/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-white shadow px-4 py-2 text-sm text-center text-gray-500">
-      &copy; {new Date().getFullYear()} e-Posyandu Care. All rights reserved. <a href="https://amadsgit.github.io/mamad-ahmad-portofolio/">Mamad Ahmad</a>
+      &copy; {new Date().getFullYear()} e-Posyandu Care. All rights reserved. <a href="https://amadsgit.github.io/mamad-ahmad-portofolio/" target="_blank"><span className="text-blue-500">Mamad Ahmad</span></a>
     </footer>
   );
 }
--- components/footer-landing.tsx
+++ components/footer-landing.tsx
@@ -2,7 +2,7 @@ export default function FooterLanding() {
   return (
     <footer className="bg-gray-100 dark:bg-gray-900 text-center py-4 mt-10 text-sm text-gray-600 dark:text-gray-300 border-t border-gray-200 dark:border-gray-700">
       <p>
-        &copy; {new Date().getFullYear()} <strong>JIDA <span className="text-blue-500">Jabar Istimewa Digital Academy</span></strong> — Dibuat oleh <span className="text-emerald-600 font-semibold"><a href="https://amadsgit.github.io/mamad-ahmad-portofolio/">Mamad Ahmad</a></span>
+        &copy; {new Date().getFullYear()} <strong>JIDA <span className="text-emerald-500">Jabar Istimewa Digital Academy</span></strong> — Dibuat oleh <span className="text-blue-500 font-semibold"><a href="https://amadsgit.github.io/mamad-ahmad-portofolio/" target="_blank">Mamad Ahmad</a></span>
       </p>
     </footer>
   );
