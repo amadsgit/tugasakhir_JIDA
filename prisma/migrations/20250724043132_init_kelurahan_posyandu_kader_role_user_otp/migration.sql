a853048ea26ad49a5e749ed603037594905df494
Mamad Ahmad <mamadahmadfnc@gmail.com>
2025-07-24 11:34:31 +0700

    update project

---
+++ prisma/migrations/20250724043132_init_kelurahan_posyandu_kader_role_user_otp/migration.sql
@@ -0,0 +1,112 @@
+-- CreateEnum
+CREATE TYPE "Akreditasi" AS ENUM ('PARIPURNA', 'PRATAMA', 'MADYA', 'PURNAMA', 'MANDIRI', 'BELUM_AKREDITASI');
+
+-- CreateTable
+CREATE TABLE "Kelurahan" (
+    "id" SERIAL NOT NULL,
+    "nama" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Kelurahan_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Posyandu" (
+    "id" SERIAL NOT NULL,
+    "nama" TEXT NOT NULL,
+    "alamat" TEXT NOT NULL,
+    "wilayah" TEXT NOT NULL,
+    "kelurahanId" INTEGER,
+    "penanggungJawab" TEXT NOT NULL,
+    "noHp" TEXT NOT NULL,
+    "akreditasi" "Akreditasi" NOT NULL,
+    "longitude" DOUBLE PRECISION NOT NULL,
+    "latitude" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Posyandu_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Kader" (
+    "id" SERIAL NOT NULL,
+    "nama" TEXT NOT NULL,
+    "nik" TEXT NOT NULL,
+    "noHp" TEXT NOT NULL,
+    "alamat" TEXT NOT NULL,
+    "posyanduId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Kader_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" SERIAL NOT NULL,
+    "nama" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "nama" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "noHp" TEXT NOT NULL,
+    "noKK" TEXT,
+    "nik" TEXT NOT NULL,
+    "tanggalLahir" TIMESTAMP(3) NOT NULL,
+    "alamat" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "roleId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Otp" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "kode" TEXT NOT NULL,
+    "expiredAt" TIMESTAMP(3) NOT NULL,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Otp_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Kelurahan_nama_key" ON "Kelurahan"("nama");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Kader_nik_key" ON "Kader"("nik");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Role_nama_key" ON "Role"("nama");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_nik_key" ON "User"("nik");
+
+-- CreateIndex
+CREATE INDEX "Otp_email_idx" ON "Otp"("email");
+
+-- AddForeignKey
+ALTER TABLE "Posyandu" ADD CONSTRAINT "Posyandu_kelurahanId_fkey" FOREIGN KEY ("kelurahanId") REFERENCES "Kelurahan"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Kader" ADD CONSTRAINT "Kader_posyanduId_fkey" FOREIGN KEY ("posyanduId") REFERENCES "Posyandu"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
